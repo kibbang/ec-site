5db0084ec724f11991fabe21879976f7971c7021
kibbang <007kibbang@gmail.com>
2020-06-10 22:43:05 +0900

    Cart Delete

--- web/resources/js/pages/Cart.vue
+++ web/resources/js/pages/Cart.vue
@@ -3,7 +3,7 @@
     <h1>Cart</h1>
       <ul style="list-style: none" v-for="cart in carts" :key="cart.id">
         <li> 
-        <img class="w-100" :src="cart.image_url" width=150px, height=100px  alt />
+        <img class="w-100" :src="cart.image_url" width="150px" height="100px"  alt />
         <p> Product Name: {{cart.name}} </p>
         <p> Product Price($): {{cart.price}} </p>
         <p> Order Quantity: {{cart.quntitiy}} </p>
